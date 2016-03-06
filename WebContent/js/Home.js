329b7c662c736e6904bdeb11b9b091416a4e30cd
NikkiMadadi <nikhitha.vn1053@gmail.com>
2016-03-06 05:39:50 -0800

    5.15 before replace

--- WebContent/js/Home.js
+++ WebContent/js/Home.js
@@ -1,10 +1,14 @@
 function searchTweets(){
 var text = $("#text").val();
 var twitteCount;
-var data = new Array();
+var data1 = new Array();
 var meta = new Array();
 var tweetMedia = new Array();
+var senti1 = new Array();
 var imgsize = new Array();
+var ncount;
+var necount;
+var pcount;
     $.ajax({url: "https://search-proxy.spredfast.com/search.json?q=nbcuhackathon&filter.start=-1d&filter.finish=0&view.entities.limit=20",
     success: function(result){
     console.log(result);
@@ -30,13 +34,12 @@ var imgsize = new Array();
     h: ele.small.h,
     w: ele.small.w
     }
-    data.push(pageData);
+    data1.push(pageData);
         });
         });
         });
     });
-    console.log(data);
-$.each(data, function (index, values) {
+$.each(data1, function (index, values) {
 	    var twUrl = values.tweet.indexOf("https:"); 
 	    var tweeturl = values.tweet.substring(twUrl);
 	    var tweet = values.tweet.substring(0,twUrl);
@@ -53,7 +56,7 @@ $.each(data, function (index, values) {
 	    }
 	     
 	    });
-	    $.each(data, function (index, values) {
+	    $.each(data1, function (index, values) {
 	    var img= "<div class='col-md-4' style='padding-right=10px'><img src="+values.imgUrl+" height='200' width='255'></div><br>";
 	    $("#photos").append(img);
 	    if(index==5){
@@ -62,5 +65,46 @@ $.each(data, function (index, values) {
 	    });
     }
     });
+$.ajax({url: "https://search-proxy.spredfast.com/search.json?q=trump&filter.start=-12h&filter.finish=0&view.sentiment=true",
+	    success: function(result){
+	    console.log(result);
+	    var total = result.views.sentiment.counts.total;
+	    senti1.push(result.views.sentiment.data);
+	    console.log(senti1);
+	    $.each(senti1, function (index, values) {
+	    $.each(values, function (index, value) {
+	    if(value.name=="neutral"){
+	    ncount=value.counts.total;
+	    }else if(value.name=="negative"){
+	    necount=value.counts.total;
+	    }else if(value.name=="positive"){
+	    pcount=value.counts.total;
+	    }
+	    });
+	    });
+	    console.log("neutral"+ncount+"negative"+necount+"positve"+pcount);
+	    }
+	    });
+	    datapie = [
+	               {label: "neutal",  data: ncount, color: '#e1ab0b'},
+	               {label: "negative",  data: necount, color: '#fe0000'},
+	               {label: "positive",  data: pcount, color: '#93b40f'}
+	           ];
+
+	           $.plot($("#placeholder"), datapie, { 
+	               series: {
+	                    pie: {
+	                        show: true,
+	                        label: {
+	                            show: true,
+	                            formatter: function(label,point){
+	                                return(point.percent.toFixed(2) + '%');
+	                                
+	                            }
+	                        }
+	                   }
+	               },        
+	               legend: {show: true}
+	           });
 }
 
