12dc6f38da268b8b72df08314476c07ddb01ae8d
kranthi kandi <kranthikumar.kandi@gmail.com>
2016-03-06 09:28:12 -0800

    stable at 9:30

--- WebContent/js/Home.js
+++ WebContent/js/Home.js
@@ -1,110 +1,113 @@
 function searchTweets(){
+	$("#about").hide();
+	$("#data").empty();
 var text = $("#text").val();
 var twitteCount;
-var data1 = new Array();
+var data = new Array();
 var meta = new Array();
 var tweetMedia = new Array();
-var senti1 = new Array();
 var imgsize = new Array();
+var senti1 = new Array();
 var ncount;
 var necount;
 var pcount;
-    $.ajax({url: "https://search-proxy.spredfast.com/search.json?q=nbcuhackathon&filter.start=-1d&filter.finish=0&view.entities.limit=20",
-    success: function(result){
-    console.log(result);
-        twitteCount=result.views.entities.counts.total;
-   meta.push(result.views.entities.meta);
-    $.each(result.views.entities.data, function (index, value) {
-    tweetMedia.push(value.raw.entities.media);
-    $.each(tweetMedia, function (index, elements) {
-        $.each(elements, function (index, element) {
-        imgsize.push(element.sizes);
-        $.each(imgsize, function (index, ele) {
-        var h= ele.small.h;
-        var w= ele.small.w;
-        
-    var pageData = {
-    id:index,
-    name:value.raw.user.name,
-    img: value.raw.user.profile_image_url,
-    date:value.raw.created_at,
-    tweet: value.raw.text,
-    screenName:value.raw.user.screen_name,
-    imgUrl:element.media_url,
-    h: ele.small.h,
-    w: ele.small.w
-    }
-    data1.push(pageData);
-        });
-        });
-        });
-    });
-$.each(data1, function (index, values) {
-	    var twUrl = values.tweet.indexOf("https:"); 
-	    var tweeturl = values.tweet.substring(twUrl);
-	    var tweet = values.tweet.substring(0,twUrl);
-	    var name= "<div class='col-md-11 '><p style='font-size: 20px' ><b>"+values.name+"</b>  @"+values.screenName+"</p>";
-	    var dat = "<p>"+values.date+"</p>";
-	    var datearray = dat.split(" ");
-	    var date = name+datearray[0]+" "+datearray[1]+" "+datearray[2];
-	    var img=values.img;
-	var tweet =date+"<a href="+tweeturl+"><p>"+tweet+"</p></a>"+"<img style='height:300 ;width:250 ; '"+"src="+values.imgUrl+"><hr><br></div>";
-	    $("#data").append("<div class='col-md-1' style='border-top=solid 1px black'><img src="+img+"></div>"); 
-	    $("#data").append(tweet);
-	    if(index==5){
-	    return false;
-	    }
-	     
-	    });
-	    $.each(data1, function (index, values) {
-	    var img= "<div class='col-md-4' style='padding-right=10px'><img src="+values.imgUrl+" height='200' width='255'></div><br>";
-	    $("#photos").append(img);
-	    if(index==5){
-	return false;
-	}
-	    });
-    }
-    });
-$.ajax({url: "https://search-proxy.spredfast.com/search.json?q=trump&filter.start=-12h&filter.finish=0&view.sentiment=true",
-	    success: function(result){
-	    console.log(result);
-	    var total = result.views.sentiment.counts.total;
-	    senti1.push(result.views.sentiment.data);
-	    console.log(senti1);
-	    $.each(senti1, function (index, values) {
-	    $.each(values, function (index, value) {
-	    if(value.name=="neutral"){
-	    ncount=value.counts.total;
-	    }else if(value.name=="negative"){
-	    necount=value.counts.total;
-	    }else if(value.name=="positive"){
-	    pcount=value.counts.total;
-	    }
-	    });
-	    });
-	    console.log("neutral"+ncount+"negative"+necount+"positve"+pcount);
-	    }
-	    });
-	    datapie = [
-	               {label: "neutal",  data: ncount, color: '#e1ab0b'},
-	               {label: "negative",  data: necount, color: '#fe0000'},
-	               {label: "positive",  data: pcount, color: '#93b40f'}
-	           ];
+var url="https://search-proxy.spredfast.com/search.json?q="+text+"&filter.start=-1d&filter.finish=0&view.entities.limit=20";
+    $.ajax({url: url,
+    	success: function(result){
+    		console.log(result);
+    	    twitteCount=result.views.entities.counts.total;
+				   meta.push(result.views.entities.meta);
+    			$.each(result.views.entities.data, function (i, value) {
+    				tweetMedia.push(value.raw.entities.media);
+    				$.each(tweetMedia, function (j, imgurls) {
+    					$.each(imgurls, function (j, imgurl) {
+    					
+    				
+    				var pageData = {
+    						name:value.raw.user.name,
+    						img: value.raw.user.profile_image_url,
+    						date:value.raw.created_at,
+    						tweet: value.raw.text,
+    						screenName:value.raw.user.screen_name,
+    						imgUrl:imgurl.media_url
+    				}
+    				data.push(pageData);
+    					});
+    				});
+        					});
+    			console.log(data);
+    			console.log(tweetMedia);
+//        				});
+//        			});
+//    			});
+    			$.each(data, function (m, values) {
+    				var twUrl = values.tweet.indexOf("https:"); 
+    				var tweeturl = values.tweet.substring(twUrl);
+    				var tweet = values.tweet.substring(0,twUrl);
+    						
+    				var img=values.img;
+    				var name= "<div class='col-md-11 '><p style='font-size: 16px' ><b>"+values.name+"</b></p><p style='font-size: 12px'>@"+values.screenName+"</p>";
+					var dat = "<p>"+values.date+"</p>";
+					var datearray = dat.split(" ");
+					var date =name+ datearray[0]+" "+datearray[1]+" "+datearray[2];
+					var tweet =date+"<a href="+tweeturl+"><p>"+tweet+"</p></a><br>"+"<img src="+values.imgUrl+"></div><br>";
+					$("#data").append("<div class='col-md-1'><img src="+img+" ></div>"); 
+					$("#data").append(tweet);
+					if(m==5){
+						return false;
+					}
+    						 
+    				});
+    			$.each(data, function (n, values) {
+    				var img= "<div class='col-md-4'><img src="+values.imgUrl+"></div>";
+    				$("#imgs").append(img);
+    				if(n==5){
+						return false;
+					}
+    			});
+    			}
+    });
+    $.ajax({url: "https://search-proxy.spredfast.com/search.json?q=trump&filter.start=-12h&filter.finish=0&view.sentiment=true",
+    	success: function(result){
+    		console.log(result);
+    		var total = result.views.sentiment.counts.total;
+    		senti1.push(result.views.sentiment.data);
+    		console.log(senti1);
+    		$.each(senti1, function (o, values) {
+    			$.each(values, function (p, value) {
+    				if(value.name=="neutral"){
+    					ncount=value.counts.total;
+    				}else if(value.name=="negative"){
+    					necount=value.counts.total;
+    				}else{
+    					pcount=value.counts.total;
+    				}
+    			});
+    		});
+    		console.log("neutral"+ncount+"negative"+necount+"positve"+pcount);
+    	}
+    });
+    datapie = [
+               {label: "neutal",  data: ncount, color: '#e1ab0b'},
+               {label: "negative",  data: necount, color: '#fe0000'},
+               {label: "positive",  data: pcount, color: '#93b40f'}
+           ];
 
-	           $.plot($("#placeholder"), datapie, { 
-	               series: {
-	                    pie: {
-	                        show: true,
-	                        label: {
-	                            show: true,
-	                            formatter: function(label,point){
-	                                return(point.percent.toFixed(2) + '%');
-	                                
-	                            }
-	                        }
-	                   }
-	               },        
-	               legend: {show: true}
-	           });
+           $.plot($("#placeholder"), datapie, { 
+               series: {
+                    pie: {
+                        show: true,
+                        label: {
+                            show: true,
+                            formatter: function(label,point){
+                                return(point.percent.toFixed(2) + '%');
+                                
+                            }
+                        }
+                   }
+               },        
+               legend: {show: true}
+           });
+
 }
 
--- WebContent/js/map.js
+++ WebContent/js/map.js
@@ -16,63 +16,76 @@ $(function(){
 			'california' : {
 				latitude: 37.0000,
 				longitude: -120.0000,
+				tooltip: {content : "New York"},
                 value: "Value 1"
 			},
 			'Silicon valley' : {
 				latitude: 37.3876,
 				longitude: -122.0575,
+				tooltip: {content : "New York"},
                 value: "Value 1"
 			},
 			'Austin' : {
 				latitude: 30.2500, 
 				longitude: -97.7500,
+				tooltip: {content : "Anchorage"},
                 value: "Value 2"
 			},
 			'sf' : {
 				latitude: 37.792032,
 				longitude: -122.394613,
+				tooltip: {content : "San Francisco"},
                 value: "Value 1"
 			},
 			'ny' : {
 				latitude: 40.7127,
 				longitude: -74.0059,
+				tooltip: {content : "San Francisco"},
                 value: "Value 1"
 			},
 			'santaclara' : {
 				latitude: 37.3544,
 				longitude: -121.9692,
+				tooltip: {content : "Pahoa"},
                 value: "Value 2"
 			},
             'bayarea' : {
 				latitude:37.6910,
 				longitude: -122.3108,
+				tooltip: {content : "Los Angeles"},
                 value: "Value 1"
 			},
             'dallas' : {
 				latitude: 32.784881,
 				longitude: -96.808244,
+				tooltip: {content : "Dallas"},
                 value: "Value 2"
 			},
             'miami' : {
 				latitude: 25.789125,
 				longitude:  -80.205674,
+				tooltip: {content : "Miami"},
                 value: "Value 2"
 			},
             'sunnyvale' : {
 				latitude: 37.3711,
 				longitude:  -122.0375,
+				tooltip: {content : "Washington"},
                 value: "Value 2"
 			},
             'paloalto' : {
 				latitude: 37.4292,
 				longitude: -122.1381,
+				tooltip: {content : "Seattle"},
                 value: "Value 1"
 			},
 			'fremont' : {
 				latitude: 37.5483,
 				longitude: -121.9886,
+				tooltip: {content : "Seattle"},
                 value: "Value 1"
 			}
 		}
 	});
 });
+
